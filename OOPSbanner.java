09260991c3246cd7a745a809e27acc56b5ff21e1
amankumar973 <ak4539@srmist.edu.in>
2026-03-03 15:04:21 +0530

    UC6: print new

--- OOPSbanner.java
+++ OOPSbanner.java
@@ -1,17 +1,55 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-        String[] lines = {
-            String.join(" ", "  *** ", "  *** ", " **** ", " **** "),
-            String.join(" ", " *   *", " *   *", " *   *", " *    "),
-            String.join(" ", " *   *", " *   *", " **** ", "  *** "),
-            String.join(" ", " *   *", " *   *", " *    ", "     *"),
-            String.join(" ", "  *** ", "  *** ", " *    ", " **** ")
-        };
+        String[] banner = new String[o.length];
 
-        for (String line : lines) {
+        for (int i = 0; i < o.length; i++) {
+            // Joins the segments with 3 spaces of padding
+            banner[i] = String.join("   ", o[i], o[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    public static String[] buildO() {
+        return new String[] {
+            "  ***  ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            "  ***  "
+        };
+    }
+
+    public static String[] buildP() {
+        return new String[] {
+            " ***** ",
+            " *    *",
+            " *    *",
+            " ***** ",
+            " *     ",
+            " *     ",
+            " *     "
+        };
+    }
+
+    public static String[] buildS() {
+        return new String[] {
+            "  **** ",
+            " *     ",
+            " *     ",
+            "  ***  ",
+            "     * ",
+            "     * ",
+            " ****  "
+        };
     }
 }
